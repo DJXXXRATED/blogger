270d913786386d932cffbce9ebfa514de339a282
DJXXXRATED <101974297+DJXXXRATED@users.noreply.github.com>
2023-06-25 20:37:16 -0600

    Update script.js

--- App/script.js
+++ App/script.js
@@ -2,3 +2,13 @@ $('.song').on('click', function () {
   $('.song').removeClass('active');
   $(this).addClass('active');
 });
+
+$(document).ready(function() {
+  // Existing code
+  
+  // Add click event listener to each song
+  $('.song').on('click', function() {
+    $('.song').removeClass('active');
+    $(this).addClass('active');
+  });
+});
