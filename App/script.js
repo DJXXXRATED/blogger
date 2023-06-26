4738d10723bedd76c766eea6961f87d7a52b51d7
DJXXXRATED <101974297+DJXXXRATED@users.noreply.github.com>
2023-06-25 20:40:45 -0600

    Update script.js

--- App/script.js
+++ App/script.js
@@ -4,11 +4,16 @@ $('.song').on('click', function () {
 });
 
 $(document).ready(function() {
-  // Existing code
-  
   // Add click event listener to each song
   $('.song').on('click', function() {
     $('.song').removeClass('active');
     $(this).addClass('active');
+
+    // Get the audio source of the clicked song
+    var audioSource = $(this).find('.song-source').attr('src');
+
+    // Update the audio player source and play the song
+    $('#audio-player').attr('src', audioSource);
+    $('#audio-player')[0].play();
   });
 });
