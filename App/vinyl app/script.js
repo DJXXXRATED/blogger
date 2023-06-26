fda5e0473e3b9be74fdf59358167c6b18784bbe0
DJXXXRATED <101974297+DJXXXRATED@users.noreply.github.com>
2023-06-25 23:03:44 -0600

    Update script.js

--- App/vinyl app/script.js
+++ App/vinyl app/script.js
@@ -1,12 +1,60 @@
 (function($) {
-		$(document).ready(function() {
+  $(document).ready(function() {
+    // Playlist
+    var playlist = [
+      {
+        artist: 'Incubus',
+        album: 'Make Yourself',
+        song: 'Stellar',
+        src: 'https://drive.google.com/uc?export=download&amp;id=1XKG-jhEpq0zlgJmi0Mr1d5OQwA4Fvv98'
+      },
+      {
+        artist: 'Artist 2',
+        album: 'Album 2',
+        song: 'Song 2',
+        src: 'path/to/song2.mp3'
+      },
+      // Add more songs to the playlist as needed
+    ];
 
-			// Pause/Play functionality
-			var playButton = $('.control-play'),
-				album = $('.album');
+    // Index of the currently playing song
+    var currentSongIndex = 0;
 
-			playButton.on('click', function() {
-				$('.music-player-container').toggleClass('is-playing');
-			});
-		});
-	})(jQuery);
+    // Pause/Play functionality
+    var playButton = $('.control-play'),
+      album = $('.album');
+
+    playButton.on('click', function() {
+      $('.music-player-container').toggleClass('is-playing');
+    });
+
+    // Update the song information
+    function updateSongInfo() {
+      var currentSong = playlist[currentSongIndex];
+      $('.artist-name').text(currentSong.artist);
+      $('.album-title').text(currentSong.album);
+      $('.song-title').text(currentSong.song);
+
+      // Update audio source
+      var audio = $('audio');
+      audio.attr('src', currentSong.src);
+    }
+
+    // Next song functionality
+    var nextButton = $('.control-forwards');
+    nextButton.on('click', function() {
+      currentSongIndex = (currentSongIndex + 1) % playlist.length;
+      updateSongInfo();
+    });
+
+    // Previous song functionality
+    var backButton = $('.control-back');
+    backButton.on('click', function() {
+      currentSongIndex = (currentSongIndex - 1 + playlist.length) % playlist.length;
+      updateSongInfo();
+    });
+
+    // Initialize the first song
+    updateSongInfo();
+  });
+})(jQuery);
